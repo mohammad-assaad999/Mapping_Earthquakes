a4f5a2a065430421e5d1ed63dd4a8afd7ff4e1fd
Your NameMohammad <you@example.com>
2021-07-29 19:01:54 -0400

    Updating all the files

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -15,15 +15,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    "Satellite Streets": satelliteStreets,
-    "Streets": streets
-    };
+    "Streets": streets,
+    "Satellite": satelliteStreets
+  };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
-    layers: [satelliteStreets]
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
     })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -38,14 +38,8 @@ let myStyle = {
     weight: 2
 }          
 
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-      style: myStyle,
-      onEachFeature: function(feature, layer) {
-          layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
-      }
-  }).addTo(map);
+  L.geoJson(data).addTo(map);
 });
